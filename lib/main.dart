db92793f8a47fff6448921f3ad11e245f61e45c8
Humna Tanveer <humma02468@gmail.com>
2022-11-27 00:03:16 +0500

    Build First app and display name 11/27/2022 12:02AM

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home()
+    );
+  }
+}
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const[
+            Text(
+              'Hello Humna'
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
